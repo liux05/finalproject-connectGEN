4735793ec4ae60ace8e570e6540979fb97d2a572
celine <89039323+liux05@users.noreply.github.com>
2022-08-16 14:17:17 -0400

    homescreen-update

---
+++ finalproject-connectGEN-main/connectGEN/FeedbackViewController.swift
@@ -0,0 +1,55 @@
+//
+//  FeedbackViewController.swift
+//  connectGEN
+//
+//  Created by scholar on 8/16/22.
+//
+
+import UIKit
+
+class FeedbackViewController: UIViewController {
+
+    @IBOutlet weak var imageOne: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func oneStar(_ sender: UIButton) {
+    }
+    
+    @IBAction func twoStar(_ sender: UIButton) {
+    }
+    
+    @IBAction func threeStar(_ sender: UIButton) {
+    }
+    
+    @IBAction func fourStar(_ sender: UIButton) {
+    }
+    
+    @IBAction func fiveStar(_ sender: UIButton) {
+    }
+    
+}
+
+func setImage() {
+    imageOne: UIImage?
+    {for state: UIControl.State
+    }
+}
+    
+
